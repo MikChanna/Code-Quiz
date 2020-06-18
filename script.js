311c9e0558d83ce441dfd4cc341458fd07fd00d8
MikChanna <channa.mik@gmail.com>
2020-06-18 18:03:03 -0400

    added jQuery source and have working timer.

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-var timer = document.querySelector("timer");
-var codeHeader = document.querySelector("codeHeader");
-var codeText = document.querySelector("codeText");
+var timer = $(".timer");
+var codeHeader = $(".codeHeader");
+var codeText = $(".codeText");
+var startButton = $(".startBtn");
+
+var seconds = 120;
+var interval;
 
 function startTimer() {
-  var seconds = 120;
-
-  if ((seconds = 120)) {
-    interval = setInverval(function () {
+  if (seconds === 120) {
+    interval = setInterval(function () {
       seconds--;
 
       renderTime();
     }, 1000);
   }
+  // if (seconds === 0) {
+  // }
+  console.log(seconds);
 }
+
+function renderTime() {
+  timer.text("Timer:" + seconds);
+}
+
+startButton.on("click", startTimer);
